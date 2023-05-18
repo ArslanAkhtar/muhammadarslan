9c8a1c2b95119e4503de6327ee44e2076e4e0712
Muhammad Arslan <muhammad.arslan@iodigital.com>
2023-05-18 08:35:03 +0200

    Animation added

--- src/components/sections/Cards.tsx
+++ src/components/sections/Cards.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent } from "react";
-import { Box, Stack, Typography, Container } from "@mui/material";
+import React, { FunctionComponent, useState } from "react";
+import { Box, Stack, Typography, Container, Fade, Slide } from "@mui/material";
 import styled from "styled-components";
 import ExperienceIcon from "@mui/icons-material/LocalPolice";
 import ProjectsIcon from "@mui/icons-material/BusinessCenter";
 import AchievementsIcon from "@mui/icons-material/MilitaryTech";
+
+import VizSensor from "react-visibility-sensor";
 
 const containerWrapper = {
   paddingBottom: "40px",
@@ -28,108 +30,131 @@ const IconSize = {
 };
 
 const Cards: FunctionComponent = () => {
+  let [active, setActive] = useState(false);
+  const containerRef = React.useRef(null);
   return (
-    <Container sx={containerWrapper}>
-      <Stack spacing={2} direction="row">
-        <Box
-          sx={{
-            width: 300,
-            height: 130,
-            borderRadius: "10px",
-            bgcolor: "#222",
-            boxShadow: "11px 10px 38px hsla(0, 0%, 0%, 38%)",
-            transition: "all 0.5s ease-in-out",
-            "&:hover": {
-              backgroundColor: "primary.main",
-              opacity: [0.9, 0.8, 0.7],
-              position: "relative",
-              bottom: "20px",
-            },
-          }}
-        >
-          <InnerContainer>
-            <ExperienceIcon sx={IconSize} />
-            <ContentContainer>
-              <Typography
-                variant="h5"
-                gutterBottom
-                sx={{ fontWeight: "bold", color: "secondary.main" }}
-              >
-                12+
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                Years of Experience
-              </Typography>
-            </ContentContainer>
-          </InnerContainer>
-        </Box>
-
-        <Box
-          sx={{
-            width: 300,
-            height: 130,
-            borderRadius: "10px",
-            bgcolor: "#222",
-            boxShadow: "11px 10px 38px hsla(0, 0%, 0%, 38%)",
-            transition: "all 0.5s ease-in-out",
-            "&:hover": {
-              backgroundColor: "primary.main",
-              opacity: [0.9, 0.8, 0.7],
-              position: "relative",
-              bottom: "20px",
-            },
-          }}
-        >
-          <InnerContainer>
-            <ProjectsIcon sx={IconSize} />
-            <ContentContainer>
-              <Typography
-                variant="h5"
-                gutterBottom
-                sx={{ fontWeight: "bold", color: "secondary.main" }}
-              >
-                10+
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                Completed Projects
-              </Typography>
-            </ContentContainer>
-          </InnerContainer>
-        </Box>
-        <Box
-          sx={{
-            width: 300,
-            height: 130,
-            borderRadius: "10px",
-            bgcolor: "#222",
-            boxShadow: "11px 10px 38px hsla(0, 0%, 0%, 38%)",
-            transition: "all 0.5s ease-in-out",
-            "&:hover": {
-              backgroundColor: "primary.main",
-              opacity: [0.9, 0.8, 0.7],
-              position: "relative",
-              bottom: "20px",
-            },
-          }}
-        >
-          <InnerContainer>
-            <AchievementsIcon sx={IconSize} />
-            <ContentContainer>
-              <Typography
-                variant="h5"
-                gutterBottom
-                sx={{ fontWeight: "bold", color: "secondary.main" }}
-              >
-                3
-              </Typography>
-              <Typography variant="h6" gutterBottom>
-                Performance Awards
-              </Typography>
-            </ContentContainer>
-          </InnerContainer>
-        </Box>
-      </Stack>
-    </Container>
+    <VizSensor
+      onChange={(isVisible: boolean) => {
+        setActive(isVisible);
+      }}
+    >
+      <Container sx={containerWrapper}>
+        <Stack spacing={2} direction="row" ref={containerRef}>
+          <Slide
+            in={active}
+            timeout={200}
+            direction="left"
+            container={containerRef.current}
+          >
+            <Box
+              sx={{
+                width: 300,
+                height: 130,
+                borderRadius: "10px",
+                bgcolor: "#222",
+                boxShadow: "11px 10px 38px hsla(0, 0%, 0%, 38%)",
+                transition: "all 0.5s ease-in-out",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                  position: "relative",
+                  bottom: "20px",
+                },
+              }}
+            >
+              <InnerContainer>
+                <ExperienceIcon sx={IconSize} />
+                <ContentContainer>
+                  <Typography
+                    variant="h5"
+                    gutterBottom
+                    sx={{ fontWeight: "bold", color: "secondary.main" }}
+                  >
+                    12+
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    Years of Experience
+                  </Typography>
+                </ContentContainer>
+              </InnerContainer>
+            </Box>
+          </Slide>
+          <Fade in={active} timeout={500}>
+            <Box
+              sx={{
+                width: 300,
+                height: 130,
+                borderRadius: "10px",
+                bgcolor: "#222",
+                boxShadow: "11px 10px 38px hsla(0, 0%, 0%, 38%)",
+                transition: "all 0.5s ease-in-out",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                  position: "relative",
+                  bottom: "20px",
+                },
+              }}
+            >
+              <InnerContainer>
+                <ProjectsIcon sx={IconSize} />
+                <ContentContainer>
+                  <Typography
+                    variant="h5"
+                    gutterBottom
+                    sx={{ fontWeight: "bold", color: "secondary.main" }}
+                  >
+                    10+
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    Completed Projects
+                  </Typography>
+                </ContentContainer>
+              </InnerContainer>
+            </Box>
+          </Fade>
+          <Slide
+            in={active}
+            timeout={200}
+            direction="right"
+            container={containerRef.current}
+          >
+            <Box
+              sx={{
+                width: 300,
+                height: 130,
+                borderRadius: "10px",
+                bgcolor: "#222",
+                boxShadow: "11px 10px 38px hsla(0, 0%, 0%, 38%)",
+                transition: "all 0.5s ease-in-out",
+                "&:hover": {
+                  backgroundColor: "primary.main",
+                  opacity: [0.9, 0.8, 0.7],
+                  position: "relative",
+                  bottom: "20px",
+                },
+              }}
+            >
+              <InnerContainer>
+                <AchievementsIcon sx={IconSize} />
+                <ContentContainer>
+                  <Typography
+                    variant="h5"
+                    gutterBottom
+                    sx={{ fontWeight: "bold", color: "secondary.main" }}
+                  >
+                    3
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    Performance Awards
+                  </Typography>
+                </ContentContainer>
+              </InnerContainer>
+            </Box>
+          </Slide>
+        </Stack>
+      </Container>
+    </VizSensor>
   );
 };
 
